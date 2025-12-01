74a145b39b43c242cb39738e3b44fdef1abb6ef2
edukor03 <eduardkorotkihs@gmail.com>
2025-12-01 18:14:12 +0000

    Fix display field bug

--- project/python/scripts/process_file.py
+++ project/python/scripts/process_file.py
@@ -3,35 +3,39 @@ import sys
 import pandas as pd
 import joblib
 import json
+from datetime import datetime
 
 file_path = sys.argv[1]
 
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
-model = joblib.load(BASE_DIR + "/models/logreg.pkl")
-vectorizer = joblib.load(BASE_DIR + "/models/vectorizer.pkl")
+pipeline = joblib.load(BASE_DIR + "/models/model.pkl")
 
 df = pd.read_csv(file_path)
-
-columns = df.columns.tolist()
 
 possible = ["email", "Email", "Email Text", "text", "message"]
 column = None
 
 for c in possible:
-    if c in columns:
+    if c in df.columns:
         column = c
         break
 
-if column is None and len(columns) == 1:
-    column = columns[0]
+if column is None and len(df.columns) == 1:
+    column = df.columns[0]
 
 if column is None:
-    raise ValueError(f"No valid email column found. Columns = {columns}")
+    raise ValueError(f"No valid email column found: {df.columns}")
 
-X = vectorizer.transform(df[column])
-preds = model.predict(X)
-probs = model.predict_proba(X)[:,1]
+df[column] = df[column].astype(str)
+
+df["length"] = df[column].str.len()
+df["num_links"] = df[column].str.count('http')
+df["num_caps"] = df[column].str.count(r'[A-Z]')
+df["num_excl"] = df[column].str.count("!")
+
+preds = pipeline.predict(df[column])
+probs = pipeline.predict_proba(df[column])[:, 1]
 
 df["prediction"] = preds
 df["confidence"] = probs
@@ -40,7 +44,7 @@ stats = {
     "num_emails": len(df),
     "num_phishing": int((preds == 1).sum()),
     "num_safe": int((preds == 0).sum()),
-    "phishing_precent": round((preds == 1).mean()*100,2)
+    "phishing_percent": round((preds == 1).mean() * 100, 2)
 }
 
 output = {
